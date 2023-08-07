54e34eb3c71abba1f839a823cb13c07513e27d70
hongduccodedao <contact.hongduc@gmail.com>
2023-08-07 18:48:01 +0700

    ✨ Home Page

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,20 +1,9 @@
 import axios from "@/axios";
 
-const accessToken = () => {
-  let localStorageData = window.localStorage.getItem("persist:lemon/user");
-  if (localStorageData && typeof localStorageData === "string") {
-    localStorageData = JSON.parse(localStorageData);
-    const AToken = JSON.parse(localStorageData.token);
-    return AToken;
-  }
-};
 export const apiGetCurrentUser = async () => {
   const response = await axios({
     method: "GET",
     url: "/user/getCurrent",
-    headers: {
-      Authorization: `Bearer ${accessToken()}`,
-    },
   });
   return response.data;
 };
