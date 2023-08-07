bb29fe0fb5302396d9f8a3fda9b6aac20c428770
hongduccodedao <contact.hongduc@gmail.com>
2023-08-07 21:41:34 +0700

    Test

--- src/apis/post.ts
+++ src/apis/post.ts
@@ -3,19 +3,19 @@ import axios from "@/axios";
 export const apiGetPosts = async () => {
   const response = await axios({
     method: "GET",
-    url: `${process.env.NEXT_PUBLIC_API_URL}/post/getAll`,
+    url: "/post/getAll",
   });
-  if (response.data?.err === 0) {
-    return response.data?.data;
+  if (response.data.err === 0) {
+    return response.data.data;
   } else {
-    return response.data?.message;
+    return response.data.message;
   }
 };
 
 export const apiGetPostBySlug = async (slug: string) => {
   const response = await axios({
     method: "GET",
-    url: `${process.env.NEXT_PUBLIC_API_URL}/post/${slug}`,
+    url: `/post/${slug}`,
   });
 
   if (response.data.err === 0) {
