1d226314b9271cd288835da6b290e519011bd7fc
hongduccodedao <contact.hongduc@gmail.com>
2023-08-09 14:29:26 +0700

    ✨ Add Register Page

---
+++ src/app/register/page.tsx
@@ -0,0 +1,142 @@
+"use client";
+import React from "react";
+import { LoginWithButton, SmallHeading } from "@/components/login";
+import { InputForm, InputPassword } from "@/components/inputs";
+import Head from "next/head";
+import Link from "next/link";
+import { paths } from "@/utils/paths";
+import { toast } from "react-toastify";
+import * as apis from "@/apis";
+import { useDispatch } from "react-redux";
+
+const Page = () => {
+  const dispatch = useDispatch();
+  const [email, setEmail] = React.useState<string>("");
+  const [password, setPassword] = React.useState<string>("");
+  const [firstName, setFirstName] = React.useState<string>("");
+  const [lastName, setLastName] = React.useState<string>("");
+  const [confirmPassword, setConfirmPassword] = React.useState<string>("");
+
+  const handleRegister = async () => {
+    email.trim();
+    password.trim();
+    firstName.trim();
+    lastName.trim();
+    confirmPassword.trim();
+
+    if (
+      email === "" ||
+      password === "" ||
+      firstName === "" ||
+      lastName === "" ||
+      confirmPassword === ""
+    ) {
+      return toast.error("Please fill all fields");
+    }
+
+    if (password !== confirmPassword) {
+      return toast.error("Password and Confirm Password must be same");
+    }
+
+    if (password.length < 6) {
+      return toast.error("Password must be at least 6 characters");
+    }
+
+    const response = await apis.apiRegister(
+      email,
+      password,
+      firstName,
+      lastName,
+    );
+    if (response.err === 0) {
+      toast.success(response.message);
+    } else {
+      toast.error(response.message);
+    }
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Login | Lemon Page🍋</title>
+      </Head>
+      <div className="max-w-[700px] mx-auto bg-ctp-surface0 p-5 rounded-lg my-20">
+        <h3 className="text-center font-bold text-3xl">
+          Welcome to Lemon Code🍋
+        </h3>
+        <div className="my-3">
+          <LoginWithButton />
+        </div>
+        <SmallHeading text="Or create an account with email" />
+        <div className="flex flex-col gap-3 py-3">
+          <div className="flex items-center gap-3">
+            <InputForm
+              id={firstName}
+              type={firstName}
+              value={firstName}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setFirstName(e.target.value)
+              }
+              placeholder="First Name"
+              required={true}
+            />
+            <InputForm
+              id={lastName}
+              type={lastName}
+              value={lastName}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setLastName(e.target.value)
+              }
+              placeholder="Last Name"
+              required={true}
+            />
+          </div>
+          <InputForm
+            id={email}
+            type={email}
+            value={email}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setEmail(e.target.value)
+            }
+            placeholder="Email Address"
+            required={true}
+          />
+          <InputPassword
+            id={password}
+            value={password}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setPassword(e.target.value)
+            }
+            placeholder="Password"
+            required={true}
+          />
+          <InputPassword
+            id={confirmPassword}
+            value={confirmPassword}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setConfirmPassword(e.target.value)
+            }
+            placeholder="Confirm Password"
+            required={true}
+          />
+        </div>
+        <div className="text-center">
+          <button
+            className="flex items-center justify-center w-full bg-ctp-green text-ctp-base py-2 text-lg rounded-md"
+            onClick={() => handleRegister()}
+          >
+            Register
+          </button>
+          <span className="pt-3">
+            Already have an account?{" "}
+            <Link href={paths.LOGIN} className="text-ctp-green hover:underline">
+              Login
+            </Link>
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Page;
