cd6d3ca9b6c10d3840edfb114930b623b3b28683
hongduccodedao <contact.hongduc@gmail.com>
2023-08-22 20:40:42 +0700

    🐛 Handle Login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,11 +7,12 @@ import Link from "next/link";
 import { paths } from "@/utils/paths";
 import { toast } from "react-toastify";
 import * as apis from "@/apis";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { handleLoginRedux } from "@/store/user/userSlice";
 
 const Page = () => {
   const dispatch = useDispatch();
+  const { isLogged } = useSelector((state: any) => state.user);
   const [email, setEmail] = React.useState<string>("");
   const [password, setPassword] = React.useState<string>("");
 
@@ -29,7 +30,7 @@ const Page = () => {
         handleLoginRedux({
           isLogged: true,
           token: response.access_token,
-        }),
+        })
       );
       window.location.href = paths.HOME;
     } else {
@@ -42,60 +43,74 @@ const Page = () => {
       <Head>
         <title>Login | Lemon Page🍋</title>
       </Head>
-      <div className="max-w-[700px] mx-auto bg-ctp-surface0 p-5 rounded-lg my-20">
-        <h3 className="text-center font-bold text-3xl">
-          Welcome to Lemon Code🍋
-        </h3>
-        <div className="my-3">
-          <LoginWithButton />
-        </div>
-        <SmallHeading text="Have a password? Continue with your email address" />
-        <div className="flex flex-col gap-3 py-3">
-          <InputForm
-            id={email}
-            type={email}
-            value={email}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setEmail(e.target.value)
-            }
-            placeholder="Email Address"
-            required={true}
-          />
-          <InputPassword
-            id={password}
-            value={password}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setPassword(e.target.value)
-            }
-            placeholder="Password"
-            required={true}
-          />
-        </div>
-        <div className="flex items-center justify-between mb-3">
-          <div className="flex items-center gap-3">
-            <input type="checkbox" name="remember" id="remember" />
-            <label htmlFor="remember">Remember me</label>
-          </div>
-          <Link href={paths.FORGOT_PASSWORD}>Forgot password?</Link>
-        </div>
-        <div className="text-center">
-          <button
-            className="flex items-center justify-center w-full bg-ctp-green text-ctp-base py-2 text-lg rounded-md"
-            onClick={() => handleLogin()}
-          >
-            Login
-          </button>
-          <span className="pt-3">
-            Don&apos;t have an account?{" "}
+      {isLogged ? (
+        <div className="max-w-[700px] mx-auto bg-ctp-surface0 p-5 rounded-lg my-20">
+          <h3 className="text-center font-bold text-3xl">You are logged in</h3>
+          <div className="text-center">
             <Link
-              href={paths.REGISTER}
+              href={paths.DASHBOARD}
               className="text-ctp-green hover:underline"
             >
-              Create account
+              Go to Dashboard
             </Link>
-          </span>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="max-w-[700px] mx-auto bg-ctp-surface0 p-5 rounded-lg my-20">
+          <h3 className="text-center font-bold text-3xl">
+            Welcome to Lemon Code🍋
+          </h3>
+          <div className="my-3">
+            <LoginWithButton />
+          </div>
+          <SmallHeading text="Have a password? Continue with your email address" />
+          <div className="flex flex-col gap-3 py-3">
+            <InputForm
+              id={email}
+              type={email}
+              value={email}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setEmail(e.target.value)
+              }
+              placeholder="Email Address"
+              required={true}
+            />
+            <InputPassword
+              id={password}
+              value={password}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setPassword(e.target.value)
+              }
+              placeholder="Password"
+              required={true}
+            />
+          </div>
+          <div className="flex items-center justify-between mb-3">
+            <div className="flex items-center gap-3">
+              <input type="checkbox" name="remember" id="remember" />
+              <label htmlFor="remember">Remember me</label>
+            </div>
+            <Link href={paths.FORGOT_PASSWORD}>Forgot password?</Link>
+          </div>
+          <div className="text-center">
+            <button
+              className="flex items-center justify-center w-full bg-ctp-green text-ctp-base py-2 text-lg rounded-md"
+              onClick={() => handleLogin()}
+            >
+              Login
+            </button>
+            <span className="pt-3">
+              Don&apos;t have an account?{" "}
+              <Link
+                href={paths.REGISTER}
+                className="text-ctp-green hover:underline"
+              >
+                Create account
+              </Link>
+            </span>
+          </div>
+        </div>
+      )}
     </>
   );
 };
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -7,10 +7,11 @@ import Link from "next/link";
 import { paths } from "@/utils/paths";
 import { toast } from "react-toastify";
 import * as apis from "@/apis";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const Page = () => {
   const dispatch = useDispatch();
+  const { isLogged } = useSelector((state: any) => state.user);
   const [email, setEmail] = React.useState<string>("");
   const [password, setPassword] = React.useState<string>("");
   const [firstName, setFirstName] = React.useState<string>("");
@@ -46,7 +47,7 @@ const Page = () => {
       email,
       password,
       firstName,
-      lastName,
+      lastName
     );
     if (response.err === 0) {
       toast.success(response.message);
@@ -68,81 +69,98 @@ const Page = () => {
       <Head>
         <title>Login | Lemon Page🍋</title>
       </Head>
-      <div className="max-w-[700px] mx-auto bg-ctp-surface0 p-5 rounded-lg my-20">
-        <h3 className="text-center font-bold text-3xl">
-          Welcome to Lemon Code🍋
-        </h3>
-        <div className="my-3">
-          <LoginWithButton />
+      {isLogged ? (
+        <div className="max-w-[700px] mx-auto bg-ctp-surface0 p-5 rounded-lg my-20">
+          <h3 className="text-center font-bold text-3xl">You are logged in</h3>
+          <div className="text-center">
+            <Link
+              href={paths.DASHBOARD}
+              className="text-ctp-green hover:underline"
+            >
+              Go to Dashboard
+            </Link>
+          </div>
         </div>
-        <SmallHeading text="Or create an account with email" />
-        <div className="flex flex-col gap-3 py-3">
-          <div className="flex items-center gap-3">
+      ) : (
+        <div className="max-w-[700px] mx-auto bg-ctp-surface0 p-5 rounded-lg my-20">
+          <h3 className="text-center font-bold text-3xl">
+            Welcome to Lemon Code🍋
+          </h3>
+          <div className="my-3">
+            <LoginWithButton />
+          </div>
+          <SmallHeading text="Or create an account with email" />
+          <div className="flex flex-col gap-3 py-3">
+            <div className="flex items-center gap-3">
+              <InputForm
+                id={firstName}
+                type={firstName}
+                value={firstName}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setFirstName(e.target.value)
+                }
+                placeholder="First Name"
+                required={true}
+              />
+              <InputForm
+                id={lastName}
+                type={lastName}
+                value={lastName}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setLastName(e.target.value)
+                }
+                placeholder="Last Name"
+                required={true}
+              />
+            </div>
             <InputForm
-              id={firstName}
-              type={firstName}
-              value={firstName}
+              id={email}
+              type={email}
+              value={email}
               onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setFirstName(e.target.value)
+                setEmail(e.target.value)
               }
-              placeholder="First Name"
+              placeholder="Email Address"
               required={true}
             />
-            <InputForm
-              id={lastName}
-              type={lastName}
-              value={lastName}
+            <InputPassword
+              id={password}
+              value={password}
               onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setLastName(e.target.value)
+                setPassword(e.target.value)
               }
-              placeholder="Last Name"
+              placeholder="Password"
+              required={true}
+            />
+            <InputPassword
+              id={confirmPassword}
+              value={confirmPassword}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setConfirmPassword(e.target.value)
+              }
+              placeholder="Confirm Password"
               required={true}
             />
           </div>
-          <InputForm
-            id={email}
-            type={email}
-            value={email}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setEmail(e.target.value)
-            }
-            placeholder="Email Address"
-            required={true}
-          />
-          <InputPassword
-            id={password}
-            value={password}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setPassword(e.target.value)
-            }
-            placeholder="Password"
-            required={true}
-          />
-          <InputPassword
-            id={confirmPassword}
-            value={confirmPassword}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setConfirmPassword(e.target.value)
-            }
-            placeholder="Confirm Password"
-            required={true}
-          />
+          <div className="text-center">
+            <button
+              className="flex items-center justify-center w-full bg-ctp-green text-ctp-base py-2 text-lg rounded-md"
+              onClick={() => handleRegister()}
+            >
+              Register
+            </button>
+            <span className="pt-3">
+              Already have an account?{" "}
+              <Link
+                href={paths.LOGIN}
+                className="text-ctp-green hover:underline"
+              >
+                Login
+              </Link>
+            </span>
+          </div>
         </div>
-        <div className="text-center">
-          <button
-            className="flex items-center justify-center w-full bg-ctp-green text-ctp-base py-2 text-lg rounded-md"
-            onClick={() => handleRegister()}
-          >
-            Register
-          </button>
-          <span className="pt-3">
-            Already have an account?{" "}
-            <Link href={paths.LOGIN} className="text-ctp-green hover:underline">
-              Login
-            </Link>
-          </span>
-        </div>
-      </div>
+      )}
     </>
   );
 };
